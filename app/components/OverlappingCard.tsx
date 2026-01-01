ecfb745d44ad48c8cb56359f2ca91406cf42535e
shruti singh <shrutisinghskr@gmail.com>
2026-01-01 22:40:09 +0530

    overlapping card

---
+++ app/components/OverlappingCard.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+
+const OverlappingCard = () => {
+  return (
+    <div className="flex flex-row">
+      <Image
+        src={"/shruti.jpg"}
+        alt="me"
+        width={100}
+        height={20}
+        className="w-28 h-28 min-[475px]:w-32 min-[475px]:h-32 min-[500px]:w-36 min-[500px]:h-36 bg-[#F5F4F9] border border-[#E6E6E6] rounded-md overflow-hidden shadow-[0_1px_2px_rgba(0,0,0,0.04)] hover:shadow-[0_6px_6px_rgba(0,0,0,0.08)] hover:-translate-y-1 transition duration-200 ease-in-out"
+        style={{ transform: "rotate(6deg)" }}
+      />
+    </div>
+  );
+};
+
+export default OverlappingCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import OverlappingCard from "./components/OverlappingCard";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+       <OverlappingCard />
+
         <Image
           className="dark:invert"
           src="/next.svg"
